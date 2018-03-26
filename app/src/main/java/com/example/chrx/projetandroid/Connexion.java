5c7fa6bfa648411acfe2fef6d08d201adf6a46ca
Corentin Kirscher <corentin.kirscher@etu.univ-amu.fr>
2018-03-26 10:38:33 +0200

    add a wonderfull button

--- app/src/main/java/com/example/chrx/projetandroid/Connexion.java
+++ app/src/main/java/com/example/chrx/projetandroid/Connexion.java
@@ -1,7 +1,10 @@
 package com.example.chrx.projetandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Connexion extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Connexion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+
+        Button buttonMap = findViewById(R.id.buttonMap);
+        buttonMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Connexion.this, Navigation.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
