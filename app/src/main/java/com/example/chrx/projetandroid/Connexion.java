1fd131852c3955fa97ad2801f95a73a5fa5a0faa
k16015233 <corentin.kirscher@etu.univ-amu.fr>
2018-03-28 10:55:07 +0200

    Ajout du bouton notification

--- app/src/main/java/com/example/chrx/projetandroid/Connexion.java
+++ app/src/main/java/com/example/chrx/projetandroid/Connexion.java
@@ -1,10 +1,15 @@
 package com.example.chrx.projetandroid;
 
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Connexion extends AppCompatActivity {
 
@@ -13,14 +18,31 @@ public class Connexion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
 
-        Button buttonMap = findViewById(R.id.buttonMap);
-        buttonMap.setOnClickListener(new View.OnClickListener() {
+        Button buttonNotif = findViewById(R.id.buttonNotif);
+        buttonNotif.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Connexion.this, Navigation.class);
-                startActivity(intent);
+                Toast.makeText(getBaseContext(), "Notification added", Toast.LENGTH_SHORT).show();
+                createNotification();
             }
         });
+    }
+
+    private void createNotification(){
+        final NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        final Intent intentNotification = new Intent(this, Connexion.class);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(this,1 , intentNotification,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Notification.Builder builder = new Notification.Builder(this)
+                .setWhen(System.currentTimeMillis())
+                .setTicker("U just got a notif braw")
+                .setSmallIcon(R.drawable.notification)
+                .setContentTitle(getResources().getString(R.string.notification_title))
+                .setContentText(getResources().getString(R.string.notification_desc))
+                .setContentIntent(pendingIntent);
+
+        notificationManager.notify(1, builder.build());
     }
 
 }
